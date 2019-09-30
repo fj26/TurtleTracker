c2f124e2a78d3b68fbe524911c1e9fdc9e7d1757
fj26 <fanqi.jia@duke.edu>
2019-09-30 12:10:56 -0400

    Adds front matter

---
+++ ARGOSTacker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Vicky Jia (fanqi.jia@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
