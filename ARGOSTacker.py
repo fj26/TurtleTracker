fe6e38039fe6aea0c210692c0bd45b44c9456507
fj26 <fanqi.jia@duke.edu>
2019-10-02 12:13:17 -0400

    Build dictionary

--- ARGOSTacker.py
+++ ARGOSTacker.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------
-# ARGOSTracker.py
+# ARGOSTrackingTool.py
 #
 # Description: Parses a line of ARGOS tracking data 
 #
-# Created by: Vicky Jia (fanqi.jia@duke.edu)
+# Created by: John Fay (john.fay@duke.edu)
 # Created on: Fall 2019
 #--------------------------------------------------------------
 
@@ -13,15 +13,23 @@ fileName = "data/raw/SaraNoHeader.txt"
 # Open the file as a read-only file object
 fileObj = open(fileName, 'r')
 
-# Read data
-lineString = fileObj.readline()
-
-# While loop
-while lineString:
+# Read the first line from the open file object
+lineStrings = fileObj.readlines()
+print ("There are {} records in the file".format(len(lineStrings)))
     
+# Close the file object
+fileObj.close()
+
+# Create empty dictionaries
+dateDict = {}
+locationDict = {}
+
+# Use a for loop to read each line, one at a time, until the list is exhausted
+for lineString in lineStrings:
+
     # Use the split command to parse the items in lineString into a list object
     lineData = lineString.split("\t")
-    
+
     # Assign variables to specfic items in the list
     recordID = lineData[0]              # ARGOS tracking record ID
     obsDateTime = lineData[2]           # Observation date and time (combined)
@@ -30,10 +38,12 @@ while lineString:
     obsLC = lineData[3]                 # Observation Location Class
     obsLat = lineData[5]                # Observation Latitude
     obsLon = lineData[6]                # Observation Longitude
-    
-    # Print information to the user
-    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".
-           format(recordID,obsLat,obsLat,obsDate))
-    
-    # Go to next line
-    lineString = fileObj.readline()
+
+    # Add values to dictionary
+    dateDict[recordID] = obsDateTime   
+    locationDict[recordID] = (obsLat, obsLon) 
+
+# Indicate script is complete
+print ("Finished")
+
+list(locationDict.keys())[0]
